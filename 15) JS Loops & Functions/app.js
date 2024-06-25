3083fc3124933361eef45f89adfbfcfe266e7876
Harshhate7 <123614140+Harsh0713@users.noreply.github.com>
2024-06-25 11:13:27 +0530

    Add files via upload

---
+++ 15) JS Loops & Functions/app.js
@@ -0,0 +1,50 @@
+// LOOP --- for loop
+// let items =  ['milk', 'bread', 'butter', 'coffee']
+// for (let i=0; i<=3; i++ ){
+//     console.log( items[i])
+// }
+
+
+
+// functions 
+
+// function grinder(items){
+//     console.log('grinding...', items)
+// }
+
+// grinder('tomatoes')
+ 
+// grinder('potatoes')
+
+
+// function add(a,b){
+//     console.log(a+b)
+
+// }
+// add(3,9)
+// add(-5,-10)
+
+
+// function cube(a){
+//     console.log(a*a*a)
+// }
+
+// cube(3)
+
+
+//arrow functions ---> es6
+
+arrowDemo = (a) => {
+    console.log(a*a*a)
+}
+
+arrowDemo(6)
+
+
+harshhate = (n) => {
+    console.log(n*n)
+}
+
+harshhate(5)
+
+
